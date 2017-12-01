1776328860218bb5d31071e59beedcc6a940dfda
Brett Sanders <bsand08@gmail.com>
2017-12-01 08:21:07 -0800

    start on check permutations

---
+++ 1_arrays_and_strings/2_checkPermutation.js
@@ -0,0 +1,26 @@
+
+/*
+ * Given two strings, write a method to decide if one is a permutation of the other.
+ *
+ * Permutation: an "arrangement number" or "order," is a rearrangement of the elements of an ordered list into a one-to-one correspondence with itself
+ *
+ * Input: assume strings,
+ * Output: true, false
+ * Complexity:
+ * Edge Cases:
+ *
+ * Ideas:
+ * - sort and then compare string for equality
+ * */
+
+
+const checkPermutation = (str1, str2) => {
+
+};
+
+// Test Cases
+console.log(checkPermutation('hello', 'llohe'), 'should be true')
+console.log(checkPermutation('ab', 'ba'), 'should be true')
+console.log(checkPermutation('', ''), 'should be true')
+console.log(checkPermutation('hello', 'llohee'), 'should be false')
+console.log(checkPermutation('', 'a'), 'should be false')
