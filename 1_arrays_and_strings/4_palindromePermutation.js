75fa8897d5a91880fa928885181ecfdec7a27309
Brett Sanders <bsand08@gmail.com>
2017-12-03 10:57:04 -0800

    set up palindrome permutation

---
+++ 1_arrays_and_strings/4_palindromePermutation.js
@@ -0,0 +1,18 @@
+/*
+ * Description:
+ *
+ * Input:
+ * Output: bool
+ * Complexity:
+ * Edge Cases:
+ *
+ * Ideas:
+ * -
+ * */
+
+// Solution
+const palindromePerm = () => {
+}
+
+// Test Cases
+console.log(palindromePerm('Tact Coa'), 'should be true')
