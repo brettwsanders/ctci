9b2c386e1866571078b39be50dc6f5b4742ab93c
Brett Sanders <bsand08@gmail.com>
2017-12-04 08:02:54 -0500

    set up oneAway

---
+++ 1_arrays_and_strings/5_oneAway.js
@@ -0,0 +1,19 @@
+/*
+ * Description: There are three types of edits that can be performed on strings: insert a character,
+ * remove a character, or replace a character. Given two strings, write a function to check if they are
+ * one edit (or zero edits) away.
+ *
+ * Input: two strings
+ * Output: bool
+ * Complexity:
+ * Edge Cases:
+ *
+ * Ideas:
+ * -
+ * */
+
+// Solution
+const oneAway = (str1, str2) => {
+}
+
+// Test Cases
