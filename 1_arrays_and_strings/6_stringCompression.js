5d7b3dfb157f623af5e6746e8b028a20644253e2
Brett Sanders <bsand08@gmail.com>
2017-12-05 08:18:25 -0500

    set up string compression

---
+++ 1_arrays_and_strings/6_stringCompression.js
@@ -0,0 +1,22 @@
+/*
+ * Description: Implement a method to perform basic string compression using the counts
+ * of repeated characers. For example, the string aabcccccaaa would become a2b1c5a3. If the
+ * "compressed" string would not become smaller than the original string, your method should return
+ * the original string. You can assume the string has only uppercase and lowercase letters (a-z).
+ *
+ * Input:
+ * Output:
+ * Complexity:
+ * Edge Cases:
+ *
+ * Ideas:
+ * -
+ * */
+
+// Solution
+const stringComp = str => {
+};
+
+// Test Cases
+console.log(stringComp('aabcccccaaa'), 'should be a2b1c5a3');
+console.log(stringComp('abcdefg'), 'should be abcdefg');
