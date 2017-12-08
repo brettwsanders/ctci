c389a7b95e0778ad9cfaad16abf1a994dbab04f8
Brett Sanders <bsand08@gmail.com>
2017-12-08 08:28:40 -0800

    set up rotateMatrix

---
+++ 1_arrays_and_strings/7_rotateMatrix.js
@@ -0,0 +1,21 @@
+/*
+ * Description: Given an image represented by an NxN matrix, where each pixel in the image is 4
+ * bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+ *
+ * Input:
+ * Output:
+ * Complexity:
+ * Edge Cases:
+ *
+ * Ideas:
+ * -
+ * */
+
+// Solution
+const rotateMatrix = matrix => {
+  let rotatedMatrix = matrix;
+  return rotatedMatrix;
+};
+
+// Test Cases
+console.log(rotateMatrix(['a']), 'should equal ["a"]');
