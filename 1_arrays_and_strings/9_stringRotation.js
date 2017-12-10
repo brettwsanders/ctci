e5ff24b6d7f2c38c7e7666c60a36e995e2b27610
Brett Sanders <bsand08@gmail.com>
2017-12-10 09:18:42 -0800

    add test cases to stringRotation

--- 1_arrays_and_strings/9_stringRotation.js
+++ 1_arrays_and_strings/9_stringRotation.js
@@ -9,7 +9,8 @@
  * Edge Cases:
  *
  * Ideas:
- * -
+ * - s1 and s2 must be same length, same characters, same order (but rotated)
+ * - if you can find the starting char of s1 in s2 then you can rearrange s2 to see if its the same
  * */
 
 // Solution
@@ -26,3 +27,42 @@ s1 = 'waterbottle';
 s2 = 'erbottlewat';
 console.log(stringRotation(), 'should be', true);
 
+// Case 2
+s1 = 'waterbottle';
+s2 = 'erbottlewa';
+console.log(stringRotation(), 'should be', false);
+
+// Case 3
+s1 = 'waterbottle';
+s2 = 'erbotltewat';
+console.log(stringRotation(), 'should be', false);
+
+// Case 4
+s1 = '';
+s2 = '';
+console.log(stringRotation(), 'should be', true);
+
+// Case 5
+s1 = 'waterbottle';
+s2 = 'waterbottle';
+console.log(stringRotation(), 'should be', true);
+
+// Case 6
+s1 = 'waterbottle';
+s2 = 'ewaterbottl';
+console.log(stringRotation(), 'should be', true);
+
+// Case 7
+s1 = 'wwwwwwwwwwe';
+s2 = 'wwwwwwwweww';
+console.log(stringRotation(), 'should be', true);
+
+// Case 8
+s1 = 'wwwwwwwwwwe';
+s2 = 'wwwwwwwwewe';
+console.log(stringRotation(), 'should be', false);
+
+// Case 9
+s1 = 'wwwwwwwwwweee';
+s2 = 'wwwwwwweeewww';
+console.log(stringRotation(), 'should be', false);
