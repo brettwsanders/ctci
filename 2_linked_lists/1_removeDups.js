c8c773b78ddc76630d526ebdca5fbf27816df35e
Brett Sanders <bsand08@gmail.com>
2017-12-11 08:38:04 -0800

    start on remove dupes linked list

---
+++ 2_linked_lists/1_removeDups.js
@@ -0,0 +1,30 @@
+/*
+ * Description: Write code to remove duplicates from an unsorted linked list.
+ *
+ * FOLLOW UP
+ *
+ * How would you solve the problem if a temporary buffer is not allowed?
+ *
+ * Input:
+ * Output:
+ * Complexity:
+ * Edge Cases:
+ *
+ * Ideas:
+ * -
+ * */
+class Node {
+  let data;
+  let next = null;
+
+  addNode: (value) => {
+
+  }
+}
+
+// Solution
+const removeDups = list => {
+}
+
+// Test Cases
+
