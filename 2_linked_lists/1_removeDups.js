7268fd2dbea68941a981a603f4c951c38c26908b
Brett Sanders <bsand08@gmail.com>
2017-12-11 08:49:39 -0800

    set up linkedList class

--- 2_linked_lists/1_removeDups.js
+++ 2_linked_lists/1_removeDups.js
@@ -14,11 +14,28 @@
  * -
  * */
 class Node {
-  let data;
-  let next = null;
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 
-  addNode: (value) => {
+  getListAsArray() {
+    const array = [];
+    let node = this;
+    while (node !== null) {
+      array.push(node.value);
+      node = node.next;
+    }
+    return array;
+  }
 
+  appendNode(value) {
+    const node = new Node(value);
+    let end = this;
+    while(end.next !== null) {
+      end = end.next;
+    }
+    end.next = node;
   }
 }
 
@@ -27,4 +44,7 @@ const removeDups = list => {
 }
 
 // Test Cases
-
+const linkedList = new Node(7);
+linkedList.appendNode(8);
+linkedList.appendNode(1);
+console.log(linkedList.getListAsArray());
