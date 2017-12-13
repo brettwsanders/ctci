e44f374dbd4f52392d68ca9666cd96f9b7ebab7c
Brett Sanders <bsand08@gmail.com>
2017-12-12 23:45:13 -0800

    set up kthToLast linkedList classes

--- 2_linked_lists/2_kthToLast.js
+++ 2_linked_lists/2_kthToLast.js
@@ -3,14 +3,52 @@
  *
  * Input:
  * Output:
- * Complexity:
+ * Constraints:
  * Edge Cases:
  *
+ * Time Complexity:
+ * Space Complexity:
+ *
  * Ideas:
- * -
  * */
 
 // Solution
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+  }
+
+  append(value) {
+    let curr = this.head;
+    while(curr.next !== null) {
+      curr = curr.next;
+    }
+    curr.next = new Node(value);
+  }
+
+  toArray() {
+    const array = [];
+    let curr = this.head;
+    while(curr !== null) {
+      array.push(curr.value);
+      curr = curr.next;
+    }
+    return array;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 // Test Cases
-//
+let linkedList = new LinkedList(7);
+linkedList.append(8);
+linkedList.append(9);
+linkedList.append(false);
+linkedList.append('');
+console.log(linkedList.toArray());
+
