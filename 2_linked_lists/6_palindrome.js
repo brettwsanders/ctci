bcf9fa14e176da254df357203a168ee2960a2eac
Brett Sanders <bsand08@gmail.com>
2017-12-18 21:11:38 -0800

    set up another test case

--- 2_linked_lists/6_palindrome.js
+++ 2_linked_lists/6_palindrome.js
@@ -1,10 +1,10 @@
 /*
  * Description: Implement a function to check if a linked list is a palindrome
  *
- * Input:
- * Output:
- * Constraints:
- * Edge Cases:
+ * Input: linked list
+ * Output: bool
+ * Constraints: none
+ * Edge Cases: linked list of size 1, values of non num or non string
  *
  * Time Complexity:
  * Space Complexity:
@@ -47,10 +47,14 @@ class LinkedListNode {
     }
     return array;
   }
+
+  isPalindrome() {
+  }
 }
 
 // Test Cases
-const linkedList = new LinkedListNode(6);
+// Case 1
+let linkedList = new LinkedListNode(6);
 linkedList.append(7);
 linkedList.append(8);
 linkedList.append(9);
@@ -58,3 +62,15 @@ linkedList.append(10);
 console.log(linkedList.toArray(), 'should equal', [6, 7, 8, 9, 10]);
 linkedList.remove();
 console.log(linkedList.toArray(), 'should equal', [7, 8, 9, 10]);
+console.log(linkedList.isPalindrome(), 'should be', false);
+
+// Case 2
+linkedList = new LinkedListNode('r');
+linkedList.append('a');
+linkedList.append('c');
+linkedList.append('e');
+linkedList.append('c');
+linkedList.append('a');
+linkedList.append('r');
+console.log(linkedList.toArray(), 'should equal', ['r', 'a', 'c', 'e', 'c', 'a', 'r']);
+console.log(linkedList.isPalindrome(), 'should be', true);
