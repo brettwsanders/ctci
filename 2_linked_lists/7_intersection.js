d779c0a3cfaec15ba03459dec3566975e940f3fa
Brett Sanders <bsand08@gmail.com>
2017-12-19 08:07:11 -0800

    set up intersection problem

---
+++ 2_linked_lists/7_intersection.js
@@ -0,0 +1,33 @@
+/*
+ * Description: Given two (singly) linked lists, determine if the two lists intersect. Return the inter-
+ * secting node. Note that the intersection is defined based on reference, not value. That is, if the kth
+ * node of the first linked list is the exact same node (by reference) as the jth node of the second
+ * linked list, then they are intersecting.
+ *
+ * Input: two singly linked lists (at least length of 1)
+ * Output: bool
+ * Constraints:
+ * Edge cases:
+ * Ideas: How to check if reference is the same?
+ *
+ * 1 -> 2 -> 3 ->
+ *                7 -> 8 -> 9
+ * 4 -> 5 -> 6 ->
+ *
+ */
+
+class LinkedList {
+  constructor() {}
+}
+
+class Node {
+  constructor() {}
+}
+
+/*
+ * Test Cases
+ *
+ * Case 1
+ */
+
+
