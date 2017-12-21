e8fa0fb550fb032945c19cc6f18a1b17f28650c2
Brett Sanders <bsand08@gmail.com>
2017-12-21 11:07:35 -0800

    set up loop detection problem

---
+++ 2_linked_lists/8_loopDetection.js
@@ -0,0 +1,16 @@
+/*
+ * Description: Given a circular linked list, implement an algorithm hat returns the node at the beginning of the loop.
+ *
+ * DEFINITION
+ * Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so
+ * as to make a loop in the linked list.
+ *
+ * EXAMPLE
+ * Input: A -> B -> C -> D -> E -> C [the same C as earlier]
+ * Output: C
+ *
+ * Input: circular linked list (assume its always circular)
+ * Output: the node at beginning of the loop
+ * Contraints:
+ * Edge Cases:
+ */
