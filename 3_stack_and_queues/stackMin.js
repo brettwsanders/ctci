f9750d5dcbac394f92e79a444d2365f8a3d69b20
Brett Sanders <bsand08@gmail.com>
2018-01-02 06:59:37 -0800

    test cases and bug fixes

--- 3_stack_and_queues/stackMin.js
+++ 3_stack_and_queues/stackMin.js
@@ -12,27 +12,70 @@ class Stack {
     constructor(value) {
         const item = { value: value, min: value };
         this.values = [item];
-        this.min = value;
+        this._min = value;
     }
 
     pop() {
         // handle size of 0
-        if (this.values.length === 0) return;
+        if (this.values.length === 0) return false;
         // remove last element
-        this.values = this.values.splice(this.values.length - 1, 1);
-        // set this.min to new last elements min
-        this.min = this.values[this.values.length - 1].min;
+        this.values.splice(this.values.length - 1, 1);
+        // set this._min to new last elements min
+        if (this.values.length) {
+            this._min = this.values[this.values.length - 1].min;
+        } else {
+            this._min = undefined;
+        }
     }
 
     push(value) {
-        if (value < this.min) {
-            this.min = value;
+        if (value < this._min) {
+            this._min = value;
         }
-        const item = { value: value, min: this.min };
+        const item = { value: value, min: this._min };
         this.values[this.values.length] = item;
     }
 
     min() {
-        return this.min;
+        return this._min;
     }
 }
+
+// Test cases
+//
+
+let stack = new Stack(1); // 1
+console.log(stack.min(), 'should be', 1);
+
+stack.push(8);  // 1, 8
+console.log(stack.min(), 'should be', 1);
+
+stack.push(2); // 1, 8, 2
+console.log(stack.min(), 'should be', 1);
+
+stack.push(0); // 1, 8, 2, 0
+console.log(stack.min(), 'should be', 0);
+
+stack.push(-1); // 1, 8, 2, 0, -1
+console.log(stack.min(), 'should be', -1);
+
+stack.pop(); // 1, 8, 2, 0
+console.log(stack.min(), 'should be', 0);
+
+stack.push(2); // 1, 8, 2, 0, 2
+console.log(stack.min(), 'should be', 0);
+
+stack.pop(); // 1, 8, 2, 0
+console.log(stack.min(), 'should be', 0);
+
+stack.pop(); // 1, 8, 2
+console.log(stack.min(), 'should be', 1);
+
+stack.pop(); // 1, 8
+console.log(stack.min(), 'should be', 1);
+
+stack.pop(); // 1
+console.log(stack.min(), 'should be', 1);
+
+stack.pop(); // empty
+console.log(stack.min(), 'should be', undefined);
