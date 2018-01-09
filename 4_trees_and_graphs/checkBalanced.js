6a672774fc8a7e4a3f2459935ca1739377db6bf9
Brett Sanders <bsand08@gmail.com>
2018-01-08 19:50:12 -0800

    solution to checkBalanced

--- 4_trees_and_graphs/checkBalanced.js
+++ 4_trees_and_graphs/checkBalanced.js
@@ -10,6 +10,14 @@ class BinaryTreeNode {
         this._children = [];
     }
 
+    getLeft() {
+        return this._children[0];
+    }
+
+    getRight() {
+        return this._children[1];
+    }
+
     addLeft(value) {
         const node = new BinaryTreeNode(value);
         this._children[0] = node;
@@ -23,6 +31,29 @@ class BinaryTreeNode {
     }
 }
 
+const checkBalanced = bt => {
+    if (!bt) return false;
+    // function for get max depth (recursive)
+    const maxDepth = node => {
+        // if undefined input return 0;
+        if (!node) return 0;
+        // get max depth of left branch + 1
+        const maxDepthLeft = 1 + maxDepth(node.getLeft());
+        // get max depth of right branch + 1
+        const maxDepthRight = 1 + maxDepth(node.getRight());
+        // return the max
+        return Math.max(maxDepthLeft, maxDepthRight);
+    };
+ 
+    // init left and right depth counts at 0
+    // get depth of left
+    // get depth of right
+    // return depth more than 1
+    const leftMaxDepth = maxDepth(bt.getLeft());
+    const rightMaxDepth = maxDepth(bt.getRight());
+    return Math.abs(leftMaxDepth - rightMaxDepth) < 2;
+};
+
 // Test Cases
 // depth = 0
 const unbalanced = new BinaryTreeNode(0);
@@ -31,9 +62,19 @@ const b1 = unbalanced.addLeft(1);
 const b2 = unbalanced.addRight(1);
 // depth = 2
 const b3 = b1.addLeft(2);
-const b4 = b1.addReft(2);
+const b4 = b1.addRight(2);
 // depth = 3
 const b5 = b3.addLeft(3);
 const b6 = b3.addRight(3);
+
+console.log(checkBalanced(unbalanced), 'should be', false);
+console.log(checkBalanced(b1), 'should be', true);
+console.log(checkBalanced(b2), 'should be', true);
+console.log(checkBalanced(b3), 'should be', true);
+console.log(checkBalanced(b4), 'should be', true);
+console.log(checkBalanced(b5), 'should be', true);
+console.log(checkBalanced(b6), 'should be', true);
+
+
 
 
