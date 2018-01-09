2e9ceaed59fa552726d1b28641d4670f16b86b58
Brett Sanders <bsand08@gmail.com>
2018-01-09 08:47:51 -0800

    solve validateBST

--- 4_trees_and_graphs/validateBST.js
+++ 4_trees_and_graphs/validateBST.js
@@ -12,12 +12,26 @@ class BinaryTreeNode {
 
     addLeft(value) {
         this.children[0] = new BinaryTreeNode(value);
-        return this.children[0];
+        return this.left;
     }
 
     addRight(value) {
         this.children[1] = new BinaryTreeNode(value);
+        return this.right;
+    }
+
+    get left() {
+        return this.children[0];
+    }
+
+    get right() {
         return this.children[1];
+    }
+
+    get countChildren() {
+        return this.children.reduce((acc, child) => {
+            if (child !== undefined) return acc + 1;
+        }, 0);
     }
 }
 
@@ -25,17 +39,31 @@ class BinaryTreeNode {
 const isBST = btn => {
     // base case
     // if leaf or undefined, return true
+    if (btn === undefined || btn.countChildren === 0) return true;
     // if left child exists, and value is more than node, return false
+    if (btn.left !== undefined && btn.left.value > btn.value) return false;
     // if right child exists, and value is less than or equal than node, return false
+    if (btn.right !== undefined && btn.right.value <= btn.value) return false;
     // return isBST(left) && isBST(right)
+    return isBST(btn.left) && isBST(btn.right);
 };
 
 // Test Cases
-const bst = new BinaryTreeNode(4);
-const left = bst.addLeft(2);
-const right = bst.addRight(6);
+let bst = new BinaryTreeNode(4);
+let left = bst.addLeft(2);
+let right = bst.addRight(6);
 left.addLeft(1);
 left.addRight(3);
 right.addLeft(5);
 right.addRight(7);
 console.log(isBST(bst), 'should be', true);
+
+bst = new BinaryTreeNode(4);
+left = bst.addLeft(2);
+right = bst.addRight(6);
+left.addLeft(1);
+left.addRight(3);
+right.addLeft(5);
+right.addRight(4);
+console.log(isBST(bst), 'should be', false);
+
