8b078b062eee42beabceab2760e688f92e8b8f8d
Brett Sanders <bsand08@gmail.com>
2018-01-14 18:08:06 -0800

    solution to successor

--- 4_trees_and_graphs/successor.js
+++ 4_trees_and_graphs/successor.js
@@ -4,20 +4,61 @@
  */
 
 class BinarySearchTreeNode {
-    constructor(value) {
+    constructor(value, parent) {
         this.value = value;
+        this.parent = parent;
         this.children = [];
     }
 
     addLeft(value) {
-        const node = new BinarySearchTreeNode(value);
+        const node = new BinarySearchTreeNode(value, this);
         this.children[0] = node;
+        return node;
     }
 
     addRight(value) {
-        const node = new BinarySearchTreeNode(value);
+        const node = new BinarySearchTreeNode(value, this);
         this.children[1] = node;
+        return node;
     }
 }
 
+const successor = bst => {
+    // check down to right and all the way left
+    let curr = bst;
+    if (curr.children[1] !== undefined) {
+        curr = curr.children[1];
+        // check down to right
+        while (curr.children[0] !== undefined) {
+            curr = curr.children[0];
+        }
+        return curr;
+    }
+    // check up
+    let parent = curr.parent;
+    while (parent !== undefined) {
+        // if curr is less then parent, return parent
+        if (curr.value <= parent.value) return parent;
+        // check up and up again
+        parent = parent.parent;
+    }
+    // return node
+    return null;
+};
+
 // Test Cases
+let bst = new BinarySearchTreeNode(4);
+let middleLeft = bst.addLeft(2);
+let middleRight = bst.addRight(6);
+let bottomLeftLeft = middleLeft.addLeft(1);
+let bottomLeftMid = middleLeft.addRight(3);
+let bottomRightMid = middleRight.addLeft(5);
+let bottomRightRight = middleRight.addRight(7);
+
+console.log(successor(bst).value, 'should be', 5);
+console.log(successor(middleLeft).value, 'should be', 3);
+console.log(successor(middleRight).value, 'should be', 7);
+console.log(successor(bottomLeftLeft).value, 'should be', 2);
+console.log(successor(bottomLeftMid).value, 'should be', 4);
+console.log(successor(bottomRightMid).value, 'should be', 6);
+console.log(successor(bottomRightRight), 'should be', null);
