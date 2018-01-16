be4a67ff03f44401956d13f88047a0a2cd9340e3
Brett Sanders <bsand08@gmail.com>
2018-01-15 16:12:41 -0800

    add memoized solution

--- 8_recursion/tripleStep.js
+++ 8_recursion/tripleStep.js
@@ -4,15 +4,32 @@
  *
  */
 
+// Memoized
 const tripleStep = steps => {
-    if (steps < 0) return 0;
-    if (steps === 0) return 1;
-    return tripleStep(steps - 1) + tripleStep(steps - 2) + tripleStep(steps - 3);
+    const store = {};
+    const getCombosCount = steps => {
+        if (store[steps] !== undefined) return store[steps]
+        if (steps < 0) return 0;
+        if (steps === 0) return 1;
+        store[steps-1] = getCombosCount(steps - 1);
+        store[steps-2] = getCombosCount(steps - 2);
+        store[steps-3] = getCombosCount(steps - 3);
+        return store[steps-1] + store[steps-2] + store[steps-3];
+    }
+
+    return getCombosCount(steps);
 };
 
+// Brute Force
+const tripleStepBrute = steps => {
+    if (steps < 0) return 0;
+    if (steps === 0) return 1;
+    return tripleStepBrute(steps-1) + tripleStepBrute(steps-2) + tripleStepBrute(steps-3)
+}
+
 // Test Cases
-console.log(tripleStep(1), 'should be', 1);
-console.log(tripleStep(2), 'should be', 2);
-console.log(tripleStep(3), 'should be', 4);
-console.log(tripleStep(4), 'should be', 7);
+//console.log(tripleStep(1), 'should be', 1);
+//console.log(tripleStep(2), 'should be', 2);
+//console.log(tripleStep(3), 'should be', 4);
+//console.log(tripleStep(4), 'should be', 7);
 console.log(tripleStep(5), 'should be', 13);
