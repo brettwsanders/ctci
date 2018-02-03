0f2b0748c7d121c7c4394b610bd9f97dcc01184f
Brett Sanders <bsand08@gmail.com>
2018-02-03 09:50:11 -0800

    first solution to magicIndex

--- 8_recursion/magicIndex.js
+++ 8_recursion/magicIndex.js
@@ -6,6 +6,33 @@
  * Follow up: What if the values are not distinct?
  */
 
-const magicIndex = () => {};
+const findMagicIndex = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === i) return arr[i];
+    }
+    return null;
+};
 
 //Test Cases
+let a;
+let result;
+
+// Test Case 1
+a = [0, 1, 2, 3];     // all magic indeces
+result = findMagicIndex(a);
+console.log(result, 'should be', 0);
+
+// Test Case 2
+a = [-100, 1, 5, 10]; // 1 is a magic index
+result = findMagicIndex(a);
+console.log(result, 'should be', 1);
+
+// Test Case 3
+a = [-2, 0, 1, 2, 4]; // 4 is a magic index
+result = findMagicIndex(a);
+console.log(result, 'should be', 4);
+
+// Test Case 4
+a = [1, 2, 3];        // no magic indeces
+result = findMagicIndex(a);
+console.log(result, 'should be', null);
