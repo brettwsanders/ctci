ee0557d4c99b672b791788a657490945e03ec619
Brett Sanders <bsand08@gmail.com>
2018-02-03 10:31:10 -0800

    binary search, recursive solution to magicIndex

--- 8_recursion/magicIndex.js
+++ 8_recursion/magicIndex.js
@@ -14,23 +14,38 @@ const findMagicIndexBrute = arr => {
 };
 
 // use binary search and recursion
-const findMagicIndex = arr => {
+const findMagicIndex = (arr, start, end) => {
     // if no array then return null
+    if (arr.length === 0) return null;
+    if (end <= start) return null;
+
+    // set start and end if not defined
+    if (start === undefined) start = 0;
+    if (end === undefined) end = arr.length - 1;
+
     // get middle of array
+    let index = start + Math.floor(((end + 1) - start) / 2);
+    let middle = arr[index];
     // check if magic
+    if (middle === index) {
+        return middle;
     // else if less than i, take second half of array
+    } else if (middle < index) {
+        return findMagicIndex(arr, index + 1, end);
     // else if more than i, take first half of array
-    // recurse
+    } else {
+        return findMagicIndex(arr, start, index - 1);
+    }
 }
 
 //Test Cases
 let a;
 let result;
 
-// Test Case 1
+//Test Case 1
 a = [0, 1, 2, 3];     // all magic indeces
 result = findMagicIndex(a);
-console.log(result, 'should be', 0);
+console.log(result, 'should be 0, 1, 2, or 3');
 
 // Test Case 2
 a = [-100, 1, 5, 10]; // 1 is a magic index
