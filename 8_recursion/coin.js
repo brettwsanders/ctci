4cad2d050b70e93125315bc71a2c0e683fa158c0
Brett Sanders <brett.sanders@vevo.com>
2018-02-11 19:58:14 -0800

    coins wip

--- 8_recursion/coin.js
+++ 8_recursion/coin.js
@@ -9,3 +9,28 @@
  */
 
 
+const getCoinCombos = cents => {
+    const coins = [1, 5, 10, 25].sort();
+    const cache = {}; 
+    
+    const recurse = remaining => {
+       if (cache[remaining]) return cache[remaining]; // solution cached
+       if (remaining === 0) return true; // solution found
+       if (n < 0) return false; // no solution
+       const solutions = [];
+       for (let i = 0; i < coins.length; i++) {
+           const coin = coins[i];
+           const newUsed = used.slice().push(coin);
+           const newRemaining = remaining - coin;
+           const subSolutions = recurse(newRemaining);
+           solutions = solutions.concat(recurse(newRemaining));
+       }
+       if (solutions.length > 0) {
+           cache[remaining] = solutions;
+       }
+       return cache[remaining];
+    };
+
+    return recurse(cents);
+};
+
